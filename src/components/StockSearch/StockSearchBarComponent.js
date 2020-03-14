a3aa29b1497c5e3f17773405631995359ef780a1
Dhruv Sheth <dhruvnsheth@gmail.com>
2020-03-13 21:45:40 -0400

    added debouncing back to searchbar

--- src/components/StockSearch/StockSearchBarComponent.js
+++ src/components/StockSearch/StockSearchBarComponent.js
@@ -1,20 +1,28 @@
-
-import React, {useState} from "react";
-import {useHistory} from "react-router-dom";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import debounce from "lodash/debounce";
 import './StockSearchBarComponent.css';
 
+const setUrl = debounce((history, newSearch) => history.push(`/search/${newSearch}`), 1000);
 
-const StockSearchBarComponent = ({searchTerm}) => {
-    let history = useHistory();
-    return <div className="col-2"
-                class="wrap">
-      <div class ="search">
-        <input type="text"
-               placeholder="Stock Ticker"
-               class ="searchTerm"
-               onChange={e => history.push(`/search/${e.target.value}`)} value={searchTerm}/>
+const StockSearchBarComponent = ({ searchTerm }) => {
+  let history = useHistory();
+  const [curSearch, setCurSearch] = useState(
+    searchTerm
+  );
+
+  let handleChange = e => {
+    const newSearch = e.target.value;
+    setCurSearch(newSearch);
+    setUrl(history, newSearch);
+  };
+
+  return (
+    <div className="col-2" class="wrap">
+      <div class="search">
+        <input type="text" placeholder="Stock Ticker" class="searchTerm" onChange={handleChange} value={curSearch} />
       </div>
-
     </div>
+  )
 }
 export default StockSearchBarComponent
