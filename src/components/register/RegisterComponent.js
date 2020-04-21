ec7cb6c7c479dee30821280a82ad3ca8e41cd9e4
Dhruv Sheth <dhruvnsheth@gmail.com>
2020-04-21 17:11:12 -0400

    refactored for legebility

--- src/components/register/RegisterComponent.js
+++ src/components/register/RegisterComponent.js
@@ -2,30 +2,32 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { ROLES } from "../../constants";
 
+
+//TODO frontend verify username/password length
 class RegisterComponent extends Component {
   state = {
     username: "",
     password: "",
-    role: "",
+    role: "Beginner",
     verify: "",
   };
 
   updateUsername = (e) => {
     e.preventDefault();
-    this.setState({username: e.target.value})
+    this.setState({ username: e.target.value })
   };
   updatePassword = (e) => {
     e.preventDefault();
-    this.setState({password: e.target.value})
+    this.setState({ password: e.target.value })
   };
   updateVerify = (e) => {
     e.preventDefault();
-    this.setState({verify: e.target.value})
+    this.setState({ verify: e.target.value })
   };
 
   updateRole = (e) => {
     e.preventDefault();
-    this.setState({role: e.target.value})
+    this.setState({ role: e.target.value })
   };
 
   renderRoleDropdown = () => {
@@ -35,8 +37,8 @@ class RegisterComponent extends Component {
         <label htmlFor="role" className="col-sm-2 col-form-label">
           Role
         </label>
-        <div className="col-sm-10" onChange={this.updateRole}>
-          <select id="role" className="form-control" value={role}>
+        <div className="col-sm-10">
+          <select id="role" className="form-control" onChange={this.updateRole} value={role}>
             {ROLES.map(role => <option key={role} value={role}>{role}</option>)}
           </select>
         </div>
@@ -44,45 +46,80 @@ class RegisterComponent extends Component {
     )
   };
 
+  renderUsernameField = () => {
+    return (
+      <div className="form-group row">
+        <label htmlFor="username" className="col-sm-2 col-form-label">
+          Username
+        </label>
+        <div className="col-sm-10">
+          <input className="form-control"
+                 value={this.state.username}
+                 onChange={this.updateUsername}
+                 id="username"
+                 placeholder="Alice" />
+        </div>
+      </div>
+    );
+  };
+
+  renderPasswordField = () => {
+    return (
+      <div className="form-group row">
+        <label htmlFor="password" className="col-sm-2 col-form-label">
+          Password
+        </label>
+        <div className="col-sm-10">
+          <input type="password"
+                 value={this.state.password}
+                 onChange={this.updatePassword}
+                 className="form-control"
+                 id="password"
+                 placeholder="123qwe#$%" />
+        </div>
+      </div>
+    )
+  };
+
+  renderVerifyField = () => {
+    return (
+      <div className="form-group row">
+        <label htmlFor="password-verify"
+               value={this.state.verify}
+               onChange={this.updateVerify}
+               className="col-sm-2 col-form-label">
+          Verify Password
+        </label>
+        <div className="col-sm-10">
+          <input type="password" className="form-control"
+                 id="password-verify" placeholder="123qwe#$%" />
+        </div>
+      </div>
+    );
+  };
+
+  registerUser = (e) => {
+    e.preventDefault();
+    console.log({
+      role: this.state.role,
+      username: this.state.username,
+      password: this.state.password
+    })
+  };
+
   render = () => {
     return (
       <div className="container">
         <h1>Register</h1>
         {this.renderRoleDropdown()}
-        <div className="form-group row">
-          <label htmlFor="username" className="col-sm-2 col-form-label">
-            Username
-          </label>
-          <div className="col-sm-10">
-            <input className="form-control"
-                   onChange={this.updateUsername}
-                   id="username"
-                   placeholder="Alice" />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="password" className="col-sm-2 col-form-label">
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input type="password" onChange={this.updatePassword} className="form-control"
-                   id="password" placeholder="123qwe#$%" />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="password-verify" onChange={this.updateVerify} className="col-sm-2 col-form-label">
-            Verify Password
-          </label>
-          <div className="col-sm-10">
-            <input type="password" className="form-control"
-                   id="password-verify" placeholder="123qwe#$%" />
-          </div>
-        </div>
+        {this.renderUsernameField()}
+        {this.renderPasswordField()}
+        {this.renderVerifyField()}
 
         <div className="form-group row">
           <div htmlFor="register" className="col-sm-2 col-form-label" />
           <div className="col-sm-10">
-            <button className="btn btn-primary btn-block" id="register">
+            <button className="btn btn-primary btn-block" id="register" onClick={this.registerUser}>
               Sign up
             </button>
             <div className="row">
