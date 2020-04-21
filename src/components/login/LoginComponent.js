7be87d5e0cd4228bac296ff6739ef6a130101454
Dhruv Sheth <dhruvnsheth@gmail.com>
2020-04-21 17:17:45 -0400

    login componenet

--- src/components/login/LoginComponent.js
+++ src/components/login/LoginComponent.js
@@ -1,55 +1,94 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const LoginComponent = () =>
-    <div className="container">
+class LoginComponent extends Component {
+  state = {
+    username: "",
+    password: "",
+  };
+
+
+  updateUsername = (e) => {
+    e.preventDefault();
+    this.setState({ username: e.target.value })
+  };
+  updatePassword = (e) => {
+    e.preventDefault();
+    this.setState({ password: e.target.value })
+  };
+
+  renderUsernameField = () => {
+    return (
+      <div className="form-group row">
+        <label htmlFor="username" className="col-sm-2 col-form-label">
+          Username
+        </label>
+        <div className="col-sm-10">
+          <input className="form-control"
+                 value={this.state.username}
+                 onChange={this.updateUsername}
+                 id="username"
+                 placeholder="Alice" />
+        </div>
+      </div>
+    );
+  };
+
+  renderPasswordField = () => {
+    return (
+      <div className="form-group row">
+        <label htmlFor="password" className="col-sm-2 col-form-label">
+          Password
+        </label>
+        <div className="col-sm-10">
+          <input type="password"
+                 value={this.state.password}
+                 onChange={this.updatePassword}
+                 className="form-control"
+                 id="password"
+                 placeholder="123qwe#$%" />
+        </div>
+      </div>
+    )
+  };
+
+  loginUser = (e) => {
+    e.preventDefault();
+    console.log({
+      username: this.state.username,
+      password: this.state.password
+    })
+  };
+
+  render = () => {
+    return (
+      <div className="container">
         <h1>Sign In</h1>
         <form>
-            <div className="form-group row">
-                <label htmlFor="wbdv-username" className="col-sm-2 col-form-label">
-                    Username </label>
-                <div className="col-sm-10">
-                    <input className="form-control wbdv-field wbdv-username"
-                           id="wbdv-username"
-                           placeholder="Alice"/>
+          {this.renderUsernameField()}
+          {this.renderPasswordField()}
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label" />
+            <div className="col-sm-10">
+              <button className="btn btn-primary btn-block" id="register" onClick={this.loginUser}>
+                Login
+              </button>
+              <div className="row">
+                <div className="col-6">
+                  <Link to="/register">Register</Link>
                 </div>
-            </div>
-            <div className="form-group row">
-                <label htmlFor="wbdv-password" className="col-sm-2 col-form-label">
-                    Password </label>
-                <div className="col-sm-10">
-                    <input type="password" className="form-control wbdv-field wbdv-password"
-                           id="wbdv-password" placeholder="123qwe#$%"/>
+              </div>
+              <div className="row">
+                <div className="col-6">
+                  <Link to="/home">Cancel</Link>
                 </div>
+              </div>
             </div>
-            <div className="form-group row">
-                <label className="col-sm-2 col-form-label"></label>
-                <div className="col-sm-10">
-                    <a className="btn btn-primary btn-block wbdv-button wbdv-login"
-                       href="../profile"
-                       id="wbdv-login">Sign in
-                    </a>
-                    <div className="row">
-                        <div className="col-6 wbdv-link wbdv-forgot-password"
-                             id="wbdv-forgot-password">
-                            <a href="#">Forgot Password?</a>
-                        </div>
-                        <div className="col-6">
-                            <a href="../register"
-                               className="float-right wbdv-link wbdv-register"
-                               id="wbdv-register">Sign up</a>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-6">
-                            <a href="../index.html"
-                               className="wbdv-cancel"
-                               id="wbdv-cancel">Cancel</a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+          </div>
         </form>
-    </div>
+      </div>
+    );
+  }
+}
 
 export default LoginComponent;
