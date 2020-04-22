5c5965055350dabd8805e8bbcdecbfc9075c1d9b
Dhruv Sheth <dhruvnsheth@gmail.com>
2020-04-21 21:51:18 -0400

    delete from profile page

--- src/components/Profile/ProfileComponent.js
+++ src/components/Profile/ProfileComponent.js
@@ -1,35 +1,45 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import userService from "../../services/userService";
-import {deleteStock, loginUser} from "../../actions/userActions";
-import {connect} from "react-redux";
+import { deleteStock, loginUser } from "../../actions/userActions";
+import { connect } from "react-redux";
 
 class ProfileComponent extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div>
-                    {this.props.name}
-                </div>
-                <div>
-                    {this.props.stocks}
-                </div>
-            </div>
-        );
-    }
+  handleDelete = (symbol, e) => {
+    e.preventDefault();
+    const { deleteStock } = this.props;
+    deleteStock(symbol);
+  }
+
+  render() {
+    console.log(this.props.stocks);
+    return (
+      <div className="container">
+        <div>
+          {this.props.name}
+        </div>
+        <ul>
+          {this.props.stocks.map(stock => <li key={stock.symbol}>
+            {stock.symbol}
+            <button onClick={(e) => this.handleDelete(stock.symbol, e)}>Delete</button>
+          </li>)}
+        </ul>
+      </div>
+    );
+  }
 
 }
 
 const stateToPropertyMapper = (state) => ({
-    stocks: state.user.stocks,
-    name: state.user.name
+  stocks: state.user.stocks,
+  name: state.user.name
 });
 
 
 const dispatcherToPropsMapper = (dispatch) => ({
-    deleteStock: async (symbol) => {
-        await userService.deleteStock(symbol);
-        dispatch(deleteStock(symbol));
-    },
+  deleteStock: async (symbol) => {
+    await userService.deleteStock(symbol);
+    dispatch(deleteStock(symbol));
+  },
 });
 
 
